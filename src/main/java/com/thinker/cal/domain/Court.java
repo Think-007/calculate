f9e6fd6c7e7e54f832e8317d87c7fdaea6b3b359
lipengfei <xinweilpf@163.com>
2017-11-18 16:41:10 +0800

    no

---
+++ src/main/java/com/thinker/cal/domain/Court.java
@@ -0,0 +1,15 @@
+package com.thinker.cal.domain;
+
+import java.io.Serializable;
+
+public class Court implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int courtId;
+	
+
+}
