8fa30e03187cf9ccf67fb90995064abb35a17f10
Think-007 <wylipengfei@163.com>
2017-11-19 21:04:55 +0800

    no

--- src/main/java/com/thinker/cal/domain/Court.java
+++ src/main/java/com/thinker/cal/domain/Court.java
@@ -1,6 +1,7 @@
 package com.thinker.cal.domain;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Court implements Serializable {
 
@@ -8,8 +9,126 @@ public class Court implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
+	// 场地id
 	private int courtId;
-	
+
+	// 场地名称
+	private String name;
+	// 地址
+	private String address;
+	// 国家
+	private String country;
+	// 城市
+	private String city;
+
+	// 场地类型
+	private int type;
+	// 场地面积
+	private double area;
+
+	// 所属机构
+	private String owner;
+
+	private String telNumber;
+
+	private String email;
+
+	private Date createTime;
+
+	public int getCourtId() {
+		return courtId;
+	}
+
+	public void setCourtId(int courtId) {
+		this.courtId = courtId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getTelNumber() {
+		return telNumber;
+	}
+
+	public void setTelNumber(String telNumber) {
+		this.telNumber = telNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Court [courtId=" + courtId + ", name=" + name + ", address=" + address + ", country=" + country
+				+ ", city=" + city + ", type=" + type + ", area=" + area + ", owner=" + owner + ", telNumber="
+				+ telNumber + ", email=" + email + ", createTime=" + createTime + "]";
+	}
 
 }
---
+++ src/main/java/com/thinker/cal/domain/MatchRecord.java
@@ -0,0 +1,129 @@
+/*      						
+ * Copyright 2012 LPF  All rights reserved.
+ * 
+ * History:
+ * ------------------------------------------------------------------------------
+ * Date    	|  Who  		|  What  
+ * 2017年11月19日	| LPF 	| 	create the file                       
+ */
+
+package com.thinker.cal.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 
+ * 比赛记录详情，按照uid分表.
+ * 
+ * <p>
+ * 类详细描述
+ * </p>
+ * 
+ * @author LPF
+ * 
+ */
+
+public class MatchRecord implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// 记录id
+	private String id;
+
+	// 比赛名称
+	private String matchName;
+
+	// 用户id
+	private String userId;
+
+	// 比赛地点id
+	private int courtId;
+
+	// 比赛人员名称
+	private String playerName;
+
+	// 挥杆次数
+	private int swingTimes;
+
+	// 成绩
+	private int score;
+
+	// 比赛时间
+	private Date createTime;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getMatchName() {
+		return matchName;
+	}
+
+	public void setMatchName(String matchName) {
+		this.matchName = matchName;
+	}
+
+	public int getCourtId() {
+		return courtId;
+	}
+
+	public void setCourtId(int courtId) {
+		this.courtId = courtId;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getSwingTimes() {
+		return swingTimes;
+	}
+
+	public void setSwingTimes(int swingTimes) {
+		this.swingTimes = swingTimes;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	@Override
+	public String toString() {
+		return "MatchRecord [id=" + id + ", matchName=" + matchName + ", userId=" + userId + ", courtId=" + courtId
+				+ ", playerName=" + playerName + ", swingTimes=" + swingTimes + ", score=" + score + ", createTime="
+				+ createTime + "]";
+	}
+
+}
